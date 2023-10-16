ce5c093baf05e196148cea2eff15610c79c55d20
Mightinity <yudistira.bimo1312@gmail.com>
2023-10-16 08:05:38 +0700

    Split to Features CPU/Memory Information

--- aannr.js
+++ aannr.js
@@ -4,8 +4,7 @@ const { aboutClient } = require('./features/whatsapp-function');
 const { generateID } = require('./features/essentials');
 const { DateTime } = require('luxon');
 const { getCTFScheduleEventData, formatDateTime } = require('./features/ctf-function');
-const os = require('os');
-const fs = require('fs')
+const { getCPUInfo, getUpTime, getMemoryInfo } = require('./features/serverinfo-function');
 const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 const chalk = require("chalk");
 const puppeteer = require("puppeteer");
@@ -165,70 +164,3 @@ client.on("message", async (msg) => {
         return;
     }
 })
-
-function getUpTime(){
-    let upTimeSeconds = os.uptime()
-    let upTimeInfo = '';
-    let upTimeDays = Math.floor(upTimeSeconds / 86400);
-    upTimeSeconds -= upTimeDays * 86400;
-    let upTimeHours = Math.floor(upTimeSeconds / 3600);
-    upTimeSeconds -= upTimeHours * 3600;
-    let upTimeMinutes = Math.floor(upTimeSeconds / 60);
-    upTimeSeconds -= upTimeMinutes * 60;
-
-    if(upTimeDays > 0){
-        upTimeInfo += `${upTimeDays} day(s) `
-    }
-    if(upTimeHours > 0){
-        upTimeInfo += `${upTimeHours} hour(s) `
-    }
-    if(upTimeMinutes > 0){
-        upTimeInfo += `${upTimeMinutes} minute(s) `
-    }
-    upTimeInfo += `${upTimeSeconds.toFixed(2)} second(s)`
-    return upTimeInfo
-}
-
-function getMemoryInfo(){
-    const memoryInfo = {
-        total: 0,
-        free: 0,
-        cache: 0
-    };
-
-    const data = fs.readFileSync("/proc/meminfo", "utf-8");
-    const lines = data.split("\n");
-
-    for (const line of lines){
-        if (line.startsWith("MemTotal")){
-            memoryInfo.total = parseInt(line.split(":")[1].trim().replace(" kB", ""));
-        } else if (line.startsWith("Cached")){
-            memoryInfo.cache = parseInt(line.split(":")[1].trim().replace(" kB", ""));
-        } else if (line.startsWith("MemFree")){
-            memoryInfo.free = parseInt(line.split(":")[1].trim().replace(" kB", ""))
-        }
-    }
-    return memoryInfo;
-}
-
-function getCPUInfo(){
-    const cpuInfo = {
-        name: 'Unknown',
-        cores: 0,
-        threads: 0
-    };
-
-    const data = fs.readFileSync("/proc/cpuinfo", "utf-8");
-    const lines = data.split("\n")
-
-    for (const line of lines){
-        if (line.startsWith("model name")){
-            cpuInfo.name = line.split(":")[1].trim();
-        } else if (line.startsWith("processor")){
-            cpuInfo.cores += 1
-        } else if (line.startsWith("siblings")){
-            cpuInfo.threads = parseInt(line.split(":")[1].trim())
-        }
-    }
-    return cpuInfo
-}
